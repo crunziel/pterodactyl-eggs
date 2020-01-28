6b7ffa0b88ccd0755a1bf9117c3289b16b3b68b9
_CrunzieL <Crunziel@zielhost.id>
2020-01-29 01:22:26 +1100

    Update setup.sh

--- Discord/NadekoBot/setup.sh
+++ Discord/NadekoBot/setup.sh
@@ -7,8 +7,8 @@ RED='\e[38;5;196m'
 #Copy all files to container from docker image
 cp -r /home/nadeko/* /home/container/
 
-echo -e "${CYAN}[ZielHost] Now you need to stop this server,go to Server Configuration, Startup and change the Startup Command to Bash run.sh"
-echo -e "${CYAN}[ZielHost] If you need assistance please contact me"
+echo "${CYAN}[ZielHost] Now you need to stop this server,go to Server Configuration, Startup and change the Startup Command to Bash run.sh"
+echo "${CYAN}[ZielHost] If you need assistance please contact me"
 
 rm setup.sh
 sleep 60s
