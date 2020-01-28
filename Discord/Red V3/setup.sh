8995b84b88a28c4b9e9200facf14aea99c77e712
_CrunzieL <Crunziel@zielhost.id>
2020-01-29 03:29:19 +1100

    Initial Commit

---
+++ Discord/Red V3/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Copy run.sh from root
+cp /run.sh /home/container/run.sh
+
+#Activate venv
+python3.8 -m venv /home/container/redenv && source home/container/redenv/bin/activate
+
+python -m pip install -U pip setuptools wheel
+python -m pip install -U Red-DiscordBot
+
+sleep 2
+
+redbot-setup
+
+echo "[ZielHost] Redbot Successfuly Configured"
+echo "[ZielHost] Please Change Your Startup Commands to 'bash run.sh'"
